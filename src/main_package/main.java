6322ea16d300c2df6bd0a8602b28fc5484658358
DarkFalconQS <dennisdenbouwmeester@f-warz.com>
2013-11-13 11:20:17 +0100

    MainProgram

---
+++ src/main_package/main.java
@@ -0,0 +1,20 @@
+package main_package;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dark_Falcon
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("test");
+    }
+}
