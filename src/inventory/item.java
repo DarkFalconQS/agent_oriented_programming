d7e35976a8f77d65c9d64add00188295e99976db
Michael van Diemen <michaelvandiemen@hotmail.com>
2013-12-04 10:09:30 +0100

    created InventoryItem
    getters and setters

--- src/inventory/item.java
+++ src/inventory/item.java
@@ -1,5 +1,32 @@
 package inventory;
 
 public class InventoryItem {
-  
+  private String itemName;
+  private int amount;
+  private Rack location;
+
+  public String getItemName() {
+    return itemName;
+  }
+
+  public int getAmount() {
+    return amount;
+  }
+
+  public Rack getLocation() {
+    return location;
+  }
+
+  public void setItemName(String name) {
+    itemName = name;
+  }
+
+  public void setAmount(int number_of) {
+    amount = number_of;
+  }
+
+  public void setLocation(Rack new_location) {
+    location = new_location;
+  }
+
 }
