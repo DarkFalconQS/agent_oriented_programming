cde682b22704a1614370d8e705239aabf718c2d4
Michael van Diemen <michaelvandiemen@hotmail.com>
2013-12-18 10:55:09 +0100

    updated filenames

--- src/order/orderPickerAgent.java
+++ src/order/orderPickerAgent.java
@@ -3,23 +3,23 @@ package order;
 import jade.core.Agent;
 
 public class OrderPickerAgent extends Agent {
-  private Queue orderList;
+  private Queue m_orderList;
 
   protected void setup() {
     
   }
 
-    public Queue getOrderList() {
-        return orderList;
-    }
+  public Queue getOrderList() {
+    return m_orderList;
+  }
 
-    public void setOrderList(Queue orderList) {
-        this.orderList = orderList;
-    }
+  public void setOrderList(Queue orderList) {
+    this.m_orderList = orderList;
+  }
 
   public OrderPickerAgent(Queue list = null) {
     if (list != null) {
-      orderList = list;
+      m_orderList = list;
     }
   }
 }
