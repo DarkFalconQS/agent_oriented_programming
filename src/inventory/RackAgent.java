6ae3c7fc4290bbbf1fe310664450487dfe4254fa
CodeAsm <nico.vijlbrief@outlook.com>
2014-01-17 21:10:45 +0100

    RackAgent Exception Fix
    Zowel legen m_items als een lege propose worden nu de agents dood niet
    meer

--- src/inventory/RackAgent.java
+++ src/inventory/RackAgent.java
@@ -16,129 +16,136 @@ import java.util.ArrayList;
  */
 public class RackAgent extends Agent {
 
-  private int m_route;
-  private String m_name;
-  private ArrayList<InventoryItem> m_items;
-  private int m_slots;
-
-  @Override
-  public void setup() {
-    
-    //ArrayList lol = new ArrayList<>();
-   	// lol.add(new InventoryItem("Xbox", 5000, 0));
-    //setItems(lol);
-     addBehaviour(new MyBehaviour(this));
-  }
-
-  public class MyBehaviour extends CyclicBehaviour {
-
-    private Agent m_a;
-    private String m_msg;
-    private String[] content_list;
-
-    public MyBehaviour(Agent a) {
-      super(a);
-      m_a = a;
-    }
+    private int m_route;
+    private String m_name;
+    private ArrayList<InventoryItem> m_items;
+    private int m_slots;
 
     @Override
-    public void action() {
-      ACLMessage msg = m_a.receive();
-      if (msg != null) {
-        
-        if (msg.getPerformative() == ACLMessage.PROPOSE) {
-            if(!msg.getContent().isEmpty()){
-            String content = msg.getContent();
-            try{
-            content_list = content.split("Name: ");
-            content_list = content_list[1].split(", Amount: ");
-            
-            InventoryItem item = new InventoryItem(content_list[0], Integer.parseInt(content_list[1]), 0); //0 word Size?
-            addItem(item);
-            }catch (Exception exc){
-                System.out.println("RackAgent: Error > " + exc.toString());
-            }         
-           // System.out.println("Name: " + item.getItemName() + ", Amount: " + item.getAmount());
-            }
-            else{
-                System.out.println("RackAgent: ProposeMessage is Empty!");
-            }
-        }
-        if (msg.getPerformative() == ACLMessage.QUERY_IF) {
-              
-            if(!getItems().isEmpty()){
-                InventoryItem item = m_items.get(0); // This gets the first element of the list, the one we just added
-                System.out.println("Name: " + item.getItemName() + ", Amount: " + item.getAmount());
+    public void setup() {
 
-            }else{
-                ACLMessage order = new ACLMessage(ACLMessage.INFORM_IF);
-                order.addReceiver(msg.getSender());
-                order.setContent("ANSWER: FALSE");
-                m_a.send(order);
-                System.out.println("Empty m_items");
-            }
-
-        }
-	msg.setReplyWith("Hi " + msg.getSender() + " from " + getLocalName());
-	m_a.send(msg);
-
-
-      }
-      block();
+    //ArrayList lol = new ArrayList<>();
+        // lol.add(new InventoryItem("Xbox", 5000, 0));
+        //setItems(lol);
+        addBehaviour(new MyBehaviour(this));
     }
-  }
 
-  public RackAgent() {
-    m_name = "name";
-    m_items = new ArrayList<>();
-    m_route = 1;
-  }
+    public class MyBehaviour extends CyclicBehaviour {
 
-  public int getRoute() {
-    return m_route;
-  }
+        private Agent m_a;
+        private String m_msg;
+        private String[] content_list;
 
-  public void setRoute(int route) {
-    this.m_route = route;
-  }
+        public MyBehaviour(Agent a) {
+            super(a);
+            m_a = a;
+        }
 
-  public String getMyName() {
-    return m_name;
-  }
+        @Override
+        public void action() {
+            ACLMessage msg = m_a.receive();
+            if (msg != null) {
 
-  public void setName(String name) {
-    this.m_name = name;
-  }
+                if (msg.getPerformative() == ACLMessage.PROPOSE) {
+                    try {
+                        if (!msg.getContent().isEmpty()) {
+                            String content = msg.getContent();
+                            try {
 
-  public int getSlots() {
-    return m_slots;
-  }
+                                content_list = content.split("Name: ");
+                                content_list = content_list[1].split(", Amount: ");
 
-  public void setSlots(int slots) {
-    this.m_slots = slots;
-  }
+                                InventoryItem item = new InventoryItem(content_list[0], Integer.parseInt(content_list[1]), 0); //0 word Size?
+                                addItem(item);
+                            } catch (Exception exc) {
+                                System.out.println("RackAgent: Error > " + exc.toString());
+                                done();
+                            }
+                            // System.out.println("Name: " + item.getItemName() + ", Amount: " + item.getAmount());
+                        } else {
+                            System.out.println("RackAgent: ProposeMessage is Empty!");
+                            done();
+                        }
+                    } catch (NullPointerException exc) {
+                        System.out.println("RackAgent: Error " + exc.toString());
+                        done();
+                }
+                }
+                if (msg.getPerformative() == ACLMessage.QUERY_IF) {
 
-  public ArrayList<InventoryItem> getItems() {
-    return m_items;
-  }
+                    if (!getItems().isEmpty()) {
+                        InventoryItem item = m_items.get(0); // This gets the first element of the list, the one we just added
+                        //System.out.println("Name: " + item.getItemName() + ", Amount: " + item.getAmount());
 
-  public void setItems(ArrayList<InventoryItem> items) {
-    this.m_items = items;
-  }
-  public void addItem(InventoryItem item) {
-    this.m_items.add(item);
-  }
+                    } else {
+                        ACLMessage order = new ACLMessage(ACLMessage.INFORM_IF);
+                        order.addReceiver(msg.getSender());
+                        order.setContent("ANSWER: FALSE");
+                        m_a.send(order);
+                        System.out.println("Empty m_items");
+                    }
 
-  // report item when amount is less then 50% of size
-  private void checkItems() {
-      for (InventoryItem item : m_items) {
-          if((item.getSize()/2) > item.getAmount()){
-              reportItem();
-          }
-      }
-  }
+                }
+                msg.setReplyWith("Hi " + msg.getSender() + " from " + getLocalName());
+                m_a.send(msg);
+                done();
+            }
+            block();
+        }
+    }
 
-  private void reportItem() {
+    public RackAgent() {
+        m_name = "name";
+        m_items = new ArrayList<>();
+        m_route = 1;
+    }
 
-  }
+    public int getRoute() {
+        return m_route;
+    }
+
+    public void setRoute(int route) {
+        this.m_route = route;
+    }
+
+    public String getMyName() {
+        return m_name;
+    }
+
+    public void setName(String name) {
+        this.m_name = name;
+    }
+
+    public int getSlots() {
+        return m_slots;
+    }
+
+    public void setSlots(int slots) {
+        this.m_slots = slots;
+    }
+
+    public ArrayList<InventoryItem> getItems() {
+        return m_items;
+    }
+
+    public void setItems(ArrayList<InventoryItem> items) {
+        this.m_items = items;
+    }
+
+    public void addItem(InventoryItem item) {
+        this.m_items.add(item);
+    }
+
+    // report item when amount is less then 50% of size
+    private void checkItems() {
+        for (InventoryItem item : m_items) {
+            if ((item.getSize() / 2) > item.getAmount()) {
+                reportItem();
+            }
+        }
+    }
+
+    private void reportItem() {
+
+    }
 }
