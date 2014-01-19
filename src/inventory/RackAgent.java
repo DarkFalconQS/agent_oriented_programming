d07b0f6ca3e307220f50e6bf9e9aa58858cd212b
CodeAsm <nico.vijlbrief@outlook.com>
2014-01-19 20:55:55 +0100

    Put Original thingys almost all back

--- src/inventory/RackAgent.java
+++ src/inventory/RackAgent.java
@@ -15,6 +15,10 @@ import java.util.ArrayList;
  *
  */
 public class RackAgent extends Agent {
+    private String m_name;
+    private ArrayList<InventoryItem> m_items;
+    private int m_route;
+    private int m_slots;
     
 @Override
 protected void setup() { //this runs once before starting behaviors
@@ -25,4 +29,58 @@ protected void setup() { //this runs once before starting behaviors
 
 	}	
 
+    public RackAgent() {
+        m_name = "name";
+        m_items = new ArrayList<>();
+        m_route = 1;
+    }
+
+    public int getRoute() {
+        return m_route;
+    }
+
+    public void setRoute(int route) {
+        this.m_route = route;
+    }
+
+    public String getMyName() {
+        return m_name;
+    }
+
+    public void setName(String name) {
+        this.m_name = name;
+    }
+
+    public int getSlots() {
+        return m_slots;
+    }
+
+    public void setSlots(int slots) {
+        this.m_slots = slots;
+    }
+
+    public ArrayList<InventoryItem> getItems() {
+        return m_items;
+    }
+
+    public void setItems(ArrayList<InventoryItem> items) {
+        this.m_items = items;
+    }
+
+    public void addItem(InventoryItem item) {
+        this.m_items.add(item);
+    }
+
+    // report item when amount is less then 50% of size
+    private void checkItems() {
+        for (InventoryItem item : m_items) {
+            if ((item.getSize() / 2) > item.getAmount()) {
+                reportItem();
+            }
+        }
+    }
+
+    private void reportItem() {
+        //TODO Of we moete hier een behavior in doen, want anders maak ik hier een ACL req in.
+    }
 }
