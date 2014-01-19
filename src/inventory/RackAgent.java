3e2c59632fadbfb5417458bf5f3e3d7bce400969
Michael van Diemen <michaelvandiemen@hotmail.com>
2014-01-19 22:26:13 +0100

    small things
    constructor of RackAgent in behaviour,  m_msg string instead of
    ACLMessage

--- src/inventory/RackAgent.java
+++ src/inventory/RackAgent.java
@@ -1,105 +1,105 @@
 package inventory;
 
 import behaviours.MessageBehaviour;
-import behaviours.PutBehaviour;
-import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import java.util.ArrayList;
 
-
 /**
- * This class has the purpose of sending, holding and recieving items
- * Asswell alerting a agent if the stock gets low
+ * This class has the purpose of sending, holding and recieving items Asswell
+ * alerting a agent if the stock gets low
+ *
  * @author Nico
  *
  */
 public class RackAgent extends Agent {
-    private String m_name;
-    private ArrayList<InventoryItem> m_items;
+
+  private String m_name;
+  private ArrayList<InventoryItem> m_items;
+  private int m_route;
+  private int m_slots;
+
+  @Override
+  protected void setup() { //this runs once before starting behaviors
+    addBehaviour(new MyBehaviour(this));
+    // First set-up answering behaviour
+  }
+
+  public class MyBehaviour extends CyclicBehaviour {
+
+    private Agent m_a;
+    private ACLMessage m_msg;
+    private String[] content_list;
     private int m_route;
-    private int m_slots;
-    
-@Override
-protected void setup() { //this runs once before starting behaviors
 
-		// First set-up answering behaviour
-	
-
-	}	
-   public class MyBehaviour extends CyclicBehaviour {
-
-        private Agent m_a;
-        private String m_msg;
-        private String[] content_list;
-        private int m_route;
-
-        public MyBehaviour(Agent a) {
-            super(a);
-            m_a = a;
-        }
-
-        @Override
-        public void action() {
-            
-            // Hier moet de magishe swith of if else bla bla 
-            addBehaviour(new MessageBehaviour(m_a));
-            System.out.println("Agent: " + m_msg.toString());
-        }    
-        
-        public MyBehaviour() {
-        m_name = "name";
-        m_items = new ArrayList<>();
-    
-        }
-   }
-    public int getRoute() {
-        return m_route;
+    public MyBehaviour(Agent a) {
+      super(a);
+      m_a = a;
+      m_msg = null;
     }
 
-    public void setRoute(int route) {
-        this.m_route = route;
-    }
+    @Override
+    public void action() {
 
-    public String getMyName() {
-        return m_name;
+      // Hier moet de magishe swith of if else bla bla
+      addBehaviour(new MessageBehaviour(m_a));
+      System.out.println("Agent: " + m_msg.toString());
     }
+  }
 
-    public void setName(String name) {
-        this.m_name = name;
-    }
+  public RackAgent() {
+    m_name = "name";
+    m_items = new ArrayList<>();
 
-    public int getSlots() {
-        return m_slots;
-    }
+  }
 
-    public void setSlots(int slots) {
-        this.m_slots = slots;
-    }
+  public int getRoute() {
+    return m_route;
+  }
 
-    public ArrayList<InventoryItem> getItems() {
-        return m_items;
-    }
+  public void setRoute(int route) {
+    this.m_route = route;
+  }
 
-    public void setItems(ArrayList<InventoryItem> items) {
-        this.m_items = items;
-    }
+  public String getMyName() {
+    return m_name;
+  }
 
-    public void addItem(InventoryItem item) {
-        this.m_items.add(item);
-    }
+  public void setName(String name) {
+    this.m_name = name;
+  }
 
-    // report item when amount is less then 50% of size
-    private void checkItems() {
-        for (InventoryItem item : m_items) {
-            if ((item.getSize() / 2) > item.getAmount()) {
-                reportItem();
-            }
-        }
-    }
+  public int getSlots() {
+    return m_slots;
+  }
 
-    private void reportItem() {
-        //TODO Of we moete hier een behavior in doen, want anders maak ik hier een ACL req in.
+  public void setSlots(int slots) {
+    this.m_slots = slots;
+  }
+
+  public ArrayList<InventoryItem> getItems() {
+    return m_items;
+  }
+
+  public void setItems(ArrayList<InventoryItem> items) {
+    this.m_items = items;
+  }
+
+  public void addItem(InventoryItem item) {
+    this.m_items.add(item);
+  }
+
+  // report item when amount is less then 50% of size
+  private void checkItems() {
+    for (InventoryItem item : m_items) {
+      if ((item.getSize() / 2) > item.getAmount()) {
+	reportItem();
+      }
     }
-    }
+  }
+
+  private void reportItem() {
+    //TODO Of we moete hier een behavior in doen, want anders maak ik hier een ACL req in.
+  }
+}
