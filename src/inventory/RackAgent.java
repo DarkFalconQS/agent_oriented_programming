2539277ab355e1a9de65b38a54a399689436e907
Michael van Diemen <michaelvandiemen@hotmail.com>
2014-01-20 01:00:33 +0100

    removed reportItem

--- src/inventory/RackAgent.java
+++ src/inventory/RackAgent.java
@@ -139,8 +139,4 @@ public class RackAgent extends Agent {
     }
     return result;
   }
-
-  private void reportItem() {
-    //TODO Of we moete hier een behavior in doen, want anders maak ik hier een ACL req in.
-  }
 }
